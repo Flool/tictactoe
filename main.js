75412878d37791c675021d26a17d61b2b0cc6320
Griffin Scott <griffinoscott@gmail.com>
2017-10-01 19:41:09 -0700

    added some variables and empty functions

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("working")
+
+/*----- constants -----*/
+var turn;
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+var squares = document.querySelector('sq1');
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function init(){
+  // squares.innerHTML = '';
+  console.log(squares)
+}
+
+function render(){
+
+  checkForWin();
+}
+
+
+function checkForWin(){
+
+}
+
+init();
