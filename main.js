cd97b58f1b54f4e71460bf76d215ac6afaba78e6
Griffin Scott <griffinoscott@gmail.com>
2017-10-01 22:12:13 -0700

    added reset button

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
 console.log("working")
 
-/*----- constants -----*/
-var turn;
 
 /*----- app's state (variables) -----*/
+var turn;
+
+
+
+/*  var squares = {
+  sq1: document.getElementsByClassName('sq1'),
+  sq2: document.getElementsByClassName('sq2'),
+  sq3: document.getElementsByClassName('sq3'),
+  sq4: document.getElementsByClassName('sq4'),
+  sq5: document.getElementsByClassName('sq5'),
+  sq6: document.getElementsByClassName('sq6'),
+  sq7: document.getElementsByClassName('sq7'),
+  sq8: document.getElementsByClassName('sq8'),
+  sq9: document.getElementsByClassName('sq9'),
+} 
+*/
 
 
 /*----- cached element references -----*/
 
-var squares = document.querySelector('sq1');
+var squares = document.getElementsByTagName('td');
+
+var sq1 = document.getElementById('sq1');
+var sq2 = document.getElementById('sq2');
+var sq3 = document.getElementById('sq3');
+var sq4 = document.getElementById('sq4');
+var sq5 = document.getElementById('sq5');
+var sq6 = document.getElementById('sq6');
+var sq7 = document.getElementById('sq7');
+var sq8 = document.getElementById('sq8');
+var sq9 = document.getElementById('sq9');
+
+var button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+  init();
+});
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
 function init(){
-  // squares.innerHTML = '';
-  console.log(squares)
+  console.log(button);
+
+  for(i=1;i <= 9; i++){
+    document.getElementById('sq' + i).innerHTML = '';
+  } 
+
+  render();
 }
 
 function render(){
@@ -24,6 +59,9 @@ function render(){
   checkForWin();
 }
 
+function handleClick(){
+  
+}
 
 function checkForWin(){
 
